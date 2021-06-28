42489ee556b27e91beea03461bb5f54f9574d9e7
alexandrefaustino <alexandrefaculdade@gmail.com>
2021-06-28 08:31:17 -0300

    inclusão de icones e configuração do campo senha

--- telas/src/pages/register/index.js
+++ telas/src/pages/register/index.js
@@ -2,6 +2,7 @@ import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
 import cadastro from '../../images/cadastro1.png';
 import gria from '../../images/gria.png';
+import { IoEyeOff } from "react-icons/io5";
 import './index.css';
 function Register() {
 
@@ -18,12 +19,24 @@ function Register() {
     });
   };
 
-  const handleClick = async () => {    
+  const handleClick = () => {    
     alert('Cadastro realizado com sucesso');
-  };
+  };  
+
+  const showPassword = () => { 
+    const tagInput = document.getElementById('password');
+    if ( tagInput.type === 'password') tagInput.type = "text";
+    else tagInput.type = "password";
+  }
+
+  const showPasswordConfirmation = () => { 
+    const tagInput = document.getElementById('confirmPassword');
+    if ( tagInput.type === 'password') tagInput.type = "text";
+    else tagInput.type = "password";
+  }
 
   return(
-    <div className="div-main">      
+    <div className="div-main">     
       <div className="div-first-content">
         <img src={ cadastro } alt="logo-cadastro" />
       </div>
@@ -39,46 +52,66 @@ function Register() {
         </div>
         
         <form className="form">
-          <input
-            className="input-cpf-cnpj"              
-            placeholder="CPF ou CNPJ"
-            name="cpf-cnpj"            
-            type="text"
-            onChange={ (event) => handleChange(event) }         
-          />
-
-          <input
-            className="input-email"              
-            placeholder="E-mail"
-            name="email"          
-            type="email"
-            onChange={ (event) => handleChange(event) }
-          /> 
-
-          <input
-            className="input-password"
-            placeholder="Senha"
-            name="password"          
-            type="password"
-            onChange={ (event) => handleChange(event) }
-          />
-
-          <input
-            className="input-confirm-password"              
-            placeholder="Confirmar Senha"
-            name="confirmPassword"          
-            type="password"
-            onChange={ (event) => handleChange(event) }
-          /> 
+          <label className="label-input" htmlFor="">
+            <input
+              className="input-cpf-cnpj"              
+              placeholder="CPF ou CNPJ"
+              name="cpf-cnpj"            
+              type="text"
+              onChange={ (event) => handleChange(event) }         
+            />
+          </label>
+          
+          <label className="label-input" htmlFor="">
+            <input
+              className="input-email"              
+              placeholder="E-mail"
+              name="email"          
+              type="email"
+              onChange={ (event) => handleChange(event) }
+            /> 
+          </label>
+          
+          <label className="label-input" htmlFor="">
+            <input
+              id="password"            
+              className="input-password"
+              placeholder="Senha"
+              name="password"          
+              type="password"
+              onChange={ (event) => handleChange(event) }
+            />
+            <IoEyeOff
+              id="icon-password" 
+              className="icon"
+              onClick={ showPassword }
+            />            
+          </label>
+          
+          <label className="label-input" htmlFor="">
+            <input              
+              id="confirmPassword"
+              className="input-confirm-password"              
+              placeholder="Confirmar Senha"
+              name="confirmPassword"          
+              type="password"
+              onChange={ (event) => handleChange(event) }
+            />
+            <IoEyeOff
+              id="icon-confirm-password"              
+              className="icon"
+              onClick={ showPasswordConfirmation }
+            />
+          </label>          
 
           <button 
-          type="button" 
-          className="button-register"
-          onClick={ handleClick }
+            type="button" 
+            className="button-register"
+            onClick={ handleClick }
           >
             CADASTRAR
           </button>   
-        </form>                     
+        </form>                             
         <Link className="link-login" to="/login">
           JÁ POSSUI CADASTRO? FAÇA LOGIN AQUI
         </Link>        
